3653282c050252ea8e2d8e0e5c5b1665aa4bd2a7
houstonsoper <houstonsoper@gmail.com>
2025-01-24 15:17:33 +0000

    Added Users table to database

--- userpanel.api/Models/User.cs
+++ userpanel.api/Models/User.cs
@@ -4,6 +4,8 @@ namespace userpanel.api.Models;
 
 public class User
 {
+    public Guid UserId { get; set; } = Guid.NewGuid();
+    
     [Required (ErrorMessage = "Username is required")]
     [StringLength(15, ErrorMessage = "Username must be between 5 and 15 characters", MinimumLength = 5)]
     public required string Username { get; set; }
